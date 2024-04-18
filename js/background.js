b69f41322fdc83d80262b2170d9df4ccdf5f1d60
Arthur <arthur.perronnet@gmail.com>
2024-04-18 22:02:28 +0200

    Creation of an array on installation

--- js/background.js
+++ js/background.js
@@ -1,3 +1,12 @@
+chrome.runtime.onInstalled.addListener(function (){
+    chrome.storage.local.set({dfn_websites: []}).then(() => {
+        chrome.storage.local.get(["dfn_websites"]).then((result) => {
+            console.log("Created the array to stock websites:");
+            console.log(result.dfn_websites);
+        });
+    });
+})
+
 chrome.runtime.onMessage.addListener( // this is the message listener
     function(request, sender, sendResponse) {
         console.log(request);
