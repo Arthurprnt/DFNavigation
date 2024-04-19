9a32c4983c848a06ce5884f680ebd30e539fac80
Arthur <arthur.perronnet@gmail.com>
2024-04-19 13:37:55 +0200

    Bug fix

--- js/content.js
+++ js/content.js
@@ -2,13 +2,15 @@ let url = document.URL;
 let siteurl = url.split("//")[1].split("/")[0];
 console.log(siteurl);
 chrome.storage.local.get(["dfn_websites"]).then((result) => {
-    if(result.dfn_websites.includes(siteurl)) {
-        console.log("The page will be blocked soon...");
-        window.stop();
-        (async () => {
-            const response = await chrome.runtime.sendMessage({message: "killedTab"});
-            console.log(response);
-          })();
-
+    for(i=0; i<result.dfn_websites.length; i+=1) {
+        if([result.dfn_websites[i], `www.${result.dfn_websites[i]}`, result.dfn_websites[i].replace("www.", "")].includes(siteurl)) {
+            console.log("The page will be blocked soon...");
+            window.stop();
+            (async () => {
+                const response = await chrome.runtime.sendMessage({message: "killedTab"});
+                console.log(response);
+              })();
+    
+        }
     }
 });
